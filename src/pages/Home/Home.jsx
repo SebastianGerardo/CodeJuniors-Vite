667b22f45836f215c34a838b35a844b0708f53ba
Sebastian-123-web <sebastian09_26@hotmail.com>
2023-04-21 23:03:49 -0500

    Error[import Header from ...]: se corrigio la ruta del archivo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Header from './pages/components/Header'
+import Header from './components/Header'
 
 const Home = () => {
   return (
