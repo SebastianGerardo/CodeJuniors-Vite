24fcc72b2af244797f46b9f2944a574a42f76019
Sebastian-123-web <sebastian09_26@hotmail.com>
2023-04-29 03:45:25 -0500

    add validacion

--- src/pages/Login/components/RegisterForm.jsx
+++ src/pages/Login/components/RegisterForm.jsx
@@ -15,7 +15,16 @@ const RegisterForm = () => {
 
 
     const [step, setStep] = useState(1);
-    const handleNextStep = (e) => {setStep(step + 1);handleSend(e);};
+    const [colorBorde, setColorBorde] = useState('');
+    const handleNextStep = (e) => {
+        if(!formData.desarrollador_email){setColorBorde('error-input'); return;}
+        if(formData.desarrollador_password){
+            if(formData.desarrollador_password !== formData.desarrollador_password2){setColorBorde('error-input'); return;}
+            setColorBorde('success-input');
+        }else{setColorBorde('error-input');return;}
+        !formData.desarrollador_nombre ? setColorBorde('error-input') : setColorBorde('success-input');
+        setStep(step + 1);
+    };
     const handlePrevStep = () => {setStep(step - 1);};
 
     return (
@@ -26,9 +35,10 @@ const RegisterForm = () => {
                         <div className='flex flex-col items-center justify-between pt-6 container h-full'>
                             <h2 className='text-lg mb-6'>Proceso de Ragistro</h2>
                             <div className='flex flex-col container'>
-                                <input type="email" name='desarrollador_email' onChange={handleChange} placeholder='Correo@algo.com' className='py-2 mb-6 px-5 rounded-lg w-3/5' />
-                                <input type="password" name='desarrollador_password' onChange={handleChange} placeholder='Ingrese una Contraseña' className='py-2 mb-6 px-5 rounded-lg w-3/5' />
-                                <input type="password" name='desarrollador_password2' onChange={handleChange} placeholder='Repita la contraseña' className='py-2 mb-6 px-5 rounded-lg w-3/5' />
+                                <input type="email" value={formData.desarrollador_email} name='desarrollador_email' onChange={handleChange} placeholder='Correo@algo.com' className={`py-2 mb-6 px-5 rounded-lg w-3/5 ${colorBorde}`} />
+                                <input type="password" value={formData.desarrollador_password} name='desarrollador_password' onChange={handleChange} placeholder='Ingrese una Contraseña' className={`py-2 mb-6 px-5 rounded-lg w-3/5 ${colorBorde}`} />
+                                <input type="password" value={formData.desarrollador_password2} name='desarrollador_password2' onChange={handleChange} placeholder='Repita la contraseña' className={`py-2 mb-6 px-5 rounded-lg w-3/5 ${colorBorde}`} />
+                                <p className='flex-none text-red-600'>Contraseña no coinciden</p>
                             </div>
                             <div className='flex flex-row justify-between container'>
                                 <button onClick={handlePrevStep} disabled className='py-3 px-5'>Anterior</button>
@@ -42,8 +52,8 @@ const RegisterForm = () => {
                         <div className='flex flex-col items-center justify-between pt-6 container h-full'>
                             <h2 className='text-lg mb-6'>Proceso de Ragistro</h2>
                             <div className='flex flex-col container'>
-                                <input type="text" name='desarrollador_nombre' onChange={handleChange} placeholder='Nombres' className='py-2 mb-6 px-5 rounded-lg w-3/5' />
-                                <input type="text" name='desarrollador_apellido' onChange={handleChange} placeholder='Apellidos' className='py-2 mb-6 px-5 rounded-lg w-3/5' />
+                                <input type="text" value={formData.desarrollador_nombre} name='desarrollador_nombre' onChange={handleChange} placeholder='Nombres' className='py-2 mb-6 px-5 rounded-lg w-3/5' />
+                                <input type="text" value={formData.desarrollador_apellido} name='desarrollador_apellido' onChange={handleChange} placeholder='Apellidos' className='py-2 mb-6 px-5 rounded-lg w-3/5' />
                                 <input type="file" placeholder='Repita la contraseña' className='py-2 mb-6 px-5 rounded-lg w-3/5' />
                             </div>
                             <div className='flex flex-row justify-between container'>
@@ -58,9 +68,9 @@ const RegisterForm = () => {
                         <div className='flex flex-col items-center justify-between pt-6 container h-full'>
                             <h2 className='text-lg mb-6'>Proceso de Ragistro</h2>
                             <div className='flex flex-col container'>
-                                <input type="number" name='desarrollador_telefono' onChange={handleChange} placeholder='Telefono' className='py-2 mb-6 px-5 rounded-lg w-3/5' />
-                                <input type="text" name='desarrollador_cargo' onChange={handleChange} placeholder='Cargo el cual se dedica' className='py-2 mb-6 px-5 rounded-lg w-3/5' />
-                                <textarea name="desarrollador_descripcion" onChange={handleChange} id="" cols="30" rows="3" className='py-2 mb-6 px-5 rounded-lg w-3/5'></textarea>
+                                <input type="number" value={formData.desarrollador_telefono} name='desarrollador_telefono' onChange={handleChange} placeholder='Telefono' className='py-2 mb-6 px-5 rounded-lg w-3/5' />
+                                <input type="text" value={formData.desarrollador_cargo} name='desarrollador_cargo' onChange={handleChange} placeholder='Cargo el cual se dedica' className='py-2 mb-6 px-5 rounded-lg w-3/5' />
+                                <textarea value={formData.desarrollador_descripcion} name="desarrollador_descripcion" onChange={handleChange} id="" cols="30" rows="3" className='py-2 mb-6 px-5 rounded-lg w-3/5'></textarea>
                             </div>
                             <div className='flex flex-row justify-between container'>
                                 <button onClick={handlePrevStep} className='py-3 px-5'>Anterior</button>
