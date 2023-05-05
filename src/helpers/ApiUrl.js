90a3b8a50b568a58fe1325864cbd9df205e2e433
Sebastián Gerardo <sebastiangerardo27@gmail.com>
2023-05-05 18:43:17 -0500

    SG | URL

--- src/helpers/ApiUrl.js
+++ src/helpers/ApiUrl.js
@@ -1,2 +1,2 @@
-//export const URL = "https://codejunior-api.onrender.com"
-export const URL = "http://localhost:3000"
+export const URL = "https://codejunior-api.onrender.com"
+// export const URL = "http://localhost:3000"
